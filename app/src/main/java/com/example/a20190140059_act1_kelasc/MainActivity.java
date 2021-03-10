b3f9a90f22ab99595afe8c6377a6b7d78b909e84
Hakeem Gufran <hakeemguran26@gmail.com>
2021-03-10 20:12:46 +0700

    Initial commit

---
+++ app/src/main/java/com/example/a20190140059_act1_kelasc/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.a20190140059_act1_kelasc;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
